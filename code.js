e7653133ce822f63f4bf7fe86c42bc8d321c1023
rithesh <ritheshr08@gmail.com>
2022-12-24 14:27:26 -0800

    6th commit

--- code.js
+++ code.js
@@ -10,3 +10,19 @@ function button(){
     document.getElementById('buttontext').innerHTML = '';
     document.getElementById('buttontext').innerHTML = options[randOptionNum];
 }
+
+function tristart() {
+    document.getElementById("buttonthing").removeAttribute('hidden')
+}
+var AreaOfTriangle = function() {
+    var base = parseFloat(document.getElementById('base').value);
+    var height = parseFloat(document.getElementById('height').value);
+
+    var areaoftri = document.getElementById('areaoftri');
+
+    var area = base * height * (1/2);
+    areaoftri.textContent = "Area = " + area;
+
+    document.getElementById("areaoftri").reset()
+    autocomplete="off"
+};
